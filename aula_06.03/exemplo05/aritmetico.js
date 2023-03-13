ad5d8b4f3c2a29be150154b95719c9d10670a80f
Giovanna <giovannadecosta@gmail.com>
2023-03-13 09:13:00 -0300

    pastas e arquivos adicionados

---
+++ aula_06.03/exemplo05/aritmetico.js
@@ -0,0 +1,15 @@
+//adicao (+)
+console.log("Soma de 2 + 1 = "+ (2+1))
+
+//subtracao (-)
+console.log("Subtração de 2 - 1 = "+ (2-1))
+
+//multiplicacao
+console.log("Multiplicação de 2 x 10 = "+ (2*10))
+
+//divisao (/)
+console.log("Divisao de 2 / 2 = "+ (2/2))
+
+//resto da divisao (%)
+console.log("Resto de 2 / 3 = "+ (2%3))
+
