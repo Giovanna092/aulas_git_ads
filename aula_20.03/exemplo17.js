49cd0aa81e4657a2432d2b8cc02b68b703d3e5df
Giovanna <giovannadecosta@gmail.com>
2023-03-20 10:54:28 -0300

    exemplos OO utilizando array e heranca

---
+++ aula_20.03/exemplo17.js
@@ -0,0 +1,24 @@
+class Livro {
+    constructor(pNome, Ppreco){
+        this.nome = pNome;
+        this.preco = Ppreco
+    }
+
+    get Nome(){return this.nome}
+    set Nome(pNome){this.nome = pNome}
+    
+    get Preco(){return this.preco}
+    set Preco(pPreco){this.preco = pPreco}
+
+
+}
+
+var obj_livro1 = new Livro("Use a cabeça Java", 200)
+var obj_livro2 = new Livro("Use a cabeça C#", 170)
+
+var lista = []
+lista.push(obj_livro1)
+lista.push(obj_livro2)
+
+console.log(lista[0])
+console.log(lista[1])
